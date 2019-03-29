bd816a3c76181c63f92980d5347ced882f878041
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2019-03-29 16:10:25 -0700

    started shooting mechanic

---
+++ Seeker/Assets/Scripts/Shot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Shot : MonoBehaviour {
+
+    public float bulletSpeed;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * bulletSpeed;
+	}
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        Collider c = collision.collider;
+
+        c.GetComponent<Rigidbody>().useGravity = false;
+        c.GetComponent<NavMeshAgent>().enabled = false;
+    }*/
+}
